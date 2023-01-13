c4291f397dce83ba89f9e4cbed34b34b8446b363
mariiatuzovska <mariia.tuzovska@gmail.com>
2023-01-13 21:20:10 +0200

    add test on limit

--- tests/e2e/cmd_engine_test.go
+++ tests/e2e/cmd_engine_test.go
@@ -12,38 +12,56 @@ func TestEngine(t *testing.T) {
 	e := newEnv()
 
 	poolName := makePoolName()
-	engineName := makeEngineName()
+	engineName1 := makeEngineName()
+	engineName2 := makeEngineName()
 
 	output := runWithAuth(t, e, fmt.Sprintf("pool create --name=%s", poolName))
 	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, poolName))
 
-	output = runWithAuth(t, e, fmt.Sprintf("engine read --name=%s", engineName))
+	output = runWithAuth(t, e, fmt.Sprintf("engine read --name=%s", engineName1))
 	requireContains(t, output, `"message": "unable to find item with specified identifier"`)
 
-	output = runWithAuth(t, e, fmt.Sprintf("engine create --name=%s --pool-name=e2e-cli-test-pool-that-does-not-exist", engineName))
+	output = runWithAuth(t, e, fmt.Sprintf("engine create --name=%s --pool-name=e2e-cli-test-pool-that-does-not-exist", engineName1))
 	requireContains(t, output, `"message": "specified pool doesn't exist"`)
 
-	output = runWithAuth(t, e, fmt.Sprintf("engine create --name=%s --pool-name=%s", engineName, poolName))
+	output = runWithAuth(t, e, fmt.Sprintf("engine create --name=%s --pool-name=%s", engineName1, poolName))
 	requireContains(t, output, fmt.Sprintf(`"createdBy": "users:%s",`, e.username))
-	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, engineName))
+	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, engineName1))
 	requireContains(t, output, fmt.Sprintf(`"poolName": "%s"`, poolName))
 
-	output = runWithAuth(t, e, fmt.Sprintf("engine read --name=%s", engineName))
+	output = runWithAuth(t, e, fmt.Sprintf("engine create --name=%s --pool-name=%s", engineName2, poolName))
 	requireContains(t, output, fmt.Sprintf(`"createdBy": "users:%s",`, e.username))
-	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, engineName))
+	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, engineName2))
+	requireContains(t, output, fmt.Sprintf(`"poolName": "%s"`, poolName))
+
+	output = runWithAuth(t, e, fmt.Sprintf("engine read --name=%s", engineName1))
+	requireContains(t, output, fmt.Sprintf(`"createdBy": "users:%s",`, e.username))
+	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, engineName1))
 	requireContains(t, output, fmt.Sprintf(`"poolName": "%s"`, poolName))
 
 	output = runWithAuth(t, e, "engine list")
 	requireContains(t, output, `"engines": [`)
-	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, engineName))
+	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, engineName1))
+	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, engineName2))
 	requireContains(t, output, fmt.Sprintf(`"poolName": "%s"`, poolName))
 
-	output = runWithAuth(t, e, fmt.Sprintf("engine delete --name=%s", engineName))
+	output = runWithAuth(t, e, "engine list --limit 1")
+	requireContains(t, output, `"engines": [`)
+	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, engineName1))
+	requireNotContains(t, output, fmt.Sprintf(`"name": "%s"`, engineName2))
+	requireContains(t, output, fmt.Sprintf(`"poolName": "%s"`, poolName))
+
+	output = runWithAuth(t, e, fmt.Sprintf("engine delete --name=%s", engineName1))
 	if output != "" {
 		t.Fatalf("Unexpected output: \n%s\n", output)
 	}
 
-	output = runWithAuth(t, e, fmt.Sprintf("engine read --name=%s", engineName))
+	output = runWithAuth(t, e, fmt.Sprintf("engine delete --name=%s", engineName2))
+	if output != "" {
+		t.Fatalf("Unexpected output: \n%s\n", output)
+	}
+
+	output = runWithAuth(t, e, fmt.Sprintf("engine read --name=%s", engineName1))
 	requireContains(t, output, `"message": "unable to find item with specified identifier"`)
 
 	runWithAuth(t, e, fmt.Sprintf("pool delete --name=%s", poolName))
