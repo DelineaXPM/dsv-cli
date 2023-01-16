f05ac707f2c8a866f58654709f2cae1e4a72f41c
mariiatuzovska <mariia.tuzovska@gmail.com>
2023-01-16 17:23:42 +0200

    add e2e test

--- tests/e2e/cmd_role_test.go
+++ tests/e2e/cmd_role_test.go
@@ -5,13 +5,17 @@ package e2e
 
 import (
 	"fmt"
+	"sort"
 	"testing"
 )
 
 func TestRole(t *testing.T) {
 	e := newEnv()
 
-	roleName := makeRoleName()
+	roleName1 := makeRoleName()
+	roleName2 := makeRoleName()
+	sortedRoleNames := []string{roleName1, roleName2}
+	sort.Strings(sortedRoleNames)
 
 	output := runWithAuth(t, e, "role")
 	requireContains(t, output, "Execute an action on a role")
@@ -22,55 +26,61 @@ func TestRole(t *testing.T) {
 	output = runWithAuth(t, e, "role create --help")
 	requireLine(t, output, "Create a role in DevOps Secrets Vault")
 
-	output = runWithAuth(t, e, fmt.Sprintf("role create --name %s --external-id some-id", roleName))
+	output = runWithAuth(t, e, fmt.Sprintf("role create --name %s --external-id some-id", roleName1))
 	requireContains(t, output, "must specify both provider and external ID")
 
-	output = runWithAuth(t, e, fmt.Sprintf("role create --name %s --provider some-provider", roleName))
+	output = runWithAuth(t, e, fmt.Sprintf("role create --name %s --provider some-provider", roleName1))
 	requireContains(t, output, "must specify both provider and external ID")
 
-	output = runWithAuth(t, e, fmt.Sprintf("role create --name %s --desc E2E-CLI-testing", roleName))
-	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName))
+	output = runWithAuth(t, e, fmt.Sprintf("role create --name %s --desc E2E-CLI-testing", roleName1))
+	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName1))
 	requireContains(t, output, `"description": "E2E-CLI-testing"`)
 
-	output = runWithAuth(t, e, fmt.Sprintf("role %s", roleName))
-	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName))
+	output = runWithAuth(t, e, fmt.Sprintf("role create --name %s --desc E2E-CLI-testing", roleName2))
+	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName2))
+	requireContains(t, output, `"description": "E2E-CLI-testing"`)
 
-	output = runWithAuth(t, e, fmt.Sprintf("role --name %s", roleName))
-	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName))
+	output = runWithAuth(t, e, fmt.Sprintf("role %s", roleName1))
+	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName1))
 
-	output = runWithAuth(t, e, fmt.Sprintf("role -n %s", roleName))
-	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName))
+	output = runWithAuth(t, e, fmt.Sprintf("role --name %s", roleName1))
+	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName1))
 
-	output = runWithAuth(t, e, fmt.Sprintf("role read --name %s", roleName))
-	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName))
+	output = runWithAuth(t, e, fmt.Sprintf("role -n %s", roleName1))
+	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName1))
 
-	output = runWithAuth(t, e, fmt.Sprintf("role read -n %s", roleName))
-	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName))
+	output = runWithAuth(t, e, fmt.Sprintf("role read --name %s", roleName1))
+	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName1))
 
-	output = runWithAuth(t, e, fmt.Sprintf("role delete -n %s", roleName))
+	output = runWithAuth(t, e, fmt.Sprintf("role read -n %s", roleName1))
+	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName1))
+
+	output = runWithAuth(t, e, fmt.Sprintf("role delete -n %s", roleName1))
 	requireContains(t, output, "role marked for deletion and will be removed in about 72 hours")
 
-	runWithAuth(t, e, fmt.Sprintf("role restore -n %s", roleName))
+	runWithAuth(t, e, fmt.Sprintf("role restore -n %s", roleName1))
 
 	output = runWithAuth(t, e, "role update --desc E2E-CLI-testing-updated")
 	requireContains(t, output, "error: must specify name")
 
-	output = runWithAuth(t, e, fmt.Sprintf("role update --name %s --desc E2E-CLI-testing-updated", roleName))
-	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName))
+	output = runWithAuth(t, e, fmt.Sprintf("role update --name %s --desc E2E-CLI-testing-updated", roleName1))
+	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName1))
 	requireContains(t, output, `"description": "E2E-CLI-testing-updated"`)
 	requireContains(t, output, `"version": "1"`)
 
-	output = runWithAuth(t, e, fmt.Sprintf("role search --query %s --limit 5", roleName[:len(roleName)-2]))
-	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName))
+	output = runWithAuth(t, e, fmt.Sprintf("role search --query %s --limit 5", roleName1))
+	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName1))
 
-	output = runWithAuth(t, e, fmt.Sprintf("role search -q %s", roleName[:len(roleName)-2]))
-	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, roleName))
+	output = runWithAuth(t, e, "role search --limit 1 --sort asc --sorted-by name --query e2e-cli-test-roles-")
+	requireContains(t, output, fmt.Sprintf(`"name": "%s"`, sortedRoleNames[0]))
 
 	output = runWithAuth(t, e, "role delete --force")
 	requireContains(t, output, "error: must specify name")
 
-	runWithAuth(t, e, fmt.Sprintf("role delete --name %s --force", roleName))
+	runWithAuth(t, e, fmt.Sprintf("role delete --name %s --force", roleName1))
 
-	output = runWithAuth(t, e, fmt.Sprintf("role delete %s --force", roleName))
+	output = runWithAuth(t, e, fmt.Sprintf("role delete %s --force", roleName1))
 	requireContains(t, output, "unable to find item with specified identifier")
+
+	runWithAuth(t, e, fmt.Sprintf("role delete --name %s --force", roleName2))
 }
