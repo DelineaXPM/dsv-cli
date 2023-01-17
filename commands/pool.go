4f8460b2d02bcefdd22ca12f710ab38eaf5bacca
mariiatuzovska <mariia.tuzovska@gmail.com>
2023-01-17 15:41:28 +0200

    rm query from pool list

--- commands/pool.go
+++ commands/pool.go
@@ -75,11 +75,9 @@ func GetPoolListCmd() (cli.Command, error) {
 		HelpText: `
 Usage:
    • pool list
-   • pool list --query mypool
    • pool list --sort asc --sorted-by name
 `,
 		FlagsPredictor: []*predictor.Params{
-			{Name: cst.Query, Shorthand: "q", Usage: "Partial search by pool name (optional)"},
 			{Name: cst.Sort, Usage: cst.SortHelpMessage, Default: "desc"},
 			{Name: cst.SortedBy, Usage: "Sort by name or created field (optional)", Default: "created"},
 		},
@@ -137,14 +135,12 @@ func handlePoolCreate(vcli vaultcli.CLI, args []string) int {
 }
 
 func handlePoolList(vcli vaultcli.CLI, args []string) int {
-	searchTerm := viper.GetString(cst.Query)
 	sort := viper.GetString(cst.Sort)
 	sortedBy := viper.GetString(cst.SortedBy)
 
 	data, apiErr := poolList(vcli, &poolListParams{
-		searchTerm: searchTerm,
-		sort:       sort,
-		sortedBy:   sortedBy,
+		sort:     sort,
+		sortedBy: sortedBy,
 	})
 	vcli.Out().WriteResponse(data, apiErr)
 	return utils.GetExecStatus(apiErr)
@@ -202,16 +198,12 @@ func poolDelete(vcli vaultcli.CLI, name string) ([]byte, *errors.ApiError) {
 }
 
 type poolListParams struct {
-	searchTerm string
-	sort       string
-	sortedBy   string
+	sort     string
+	sortedBy string
 }
 
 func poolList(vcli vaultcli.CLI, params *poolListParams) ([]byte, *errors.ApiError) {
 	queryParams := map[string]string{}
-	if params.searchTerm != "" {
-		queryParams[cst.SearchTerm] = params.searchTerm
-	}
 	if params.sort != "" {
 		queryParams[cst.Sort] = params.sort
 	}
