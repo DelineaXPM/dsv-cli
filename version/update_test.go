3f7f3d65a058373e522e6d8c8937903e96626d66
Sheldon Hull <sheldonhull@users.noreply.github.com>
2024-02-12 20:00:35 -0600

    feat(testing): improve test file handling and cleanup
    - Implement `createTestDirectory` for consistent test file locations
    - Update version data in test cases
    - Streamline generation and handling of temporary files
    - Add option to keep test files for debugging purposes
    - Adjust file deletion to timing of test case closure

--- version/update_test.go
+++ version/update_test.go
@@ -12,6 +12,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// cacheDirectory is for local files that we generate during tests so easy to review and cleanup.
+var cacheDirectory = filepath.Join("../", ".artifacts", "tests") //nolint:gochecknoglobals // for testing this is ok to have fixed output location
+// PermissionUserReadWriteExecute is the permissions for the cache directory.
+const PermissionUserReadWriteExecute = 0o0700
+
+func createTestDirectory(t *testing.T) {
+	t.Helper()
+
+	if err := os.MkdirAll(cacheDirectory, PermissionUserReadWriteExecute); err != nil {
+		t.Fatalf("failure to create cache directory: %s: err: %v", cacheDirectory, err)
+	}
+	t.Logf("cacheDirectory created: %s", cacheDirectory)
+}
+
 func TestIsVersionOutdated(t *testing.T) {
 	cases := []struct {
 		target   string
@@ -72,39 +86,42 @@ func TestReadCache(t *testing.T) {
 			name:     "Old cached content",
 			filename: "old_cached_content_*.json",
 			// content:  time.Now().Add(checkFrequencyDays*-2*time.Hour*24).Format(dateLayout) + "\nWrong JSON string",
-			content: time.Now().Add(checkFrequencyDays*-2*time.Hour*24).Format(dateLayout) + `
-{"latest":"1.29.0","links": {"darwin/amd64":"https://dsv.secretsvaultcloud.com/downloads/cli/1.29.0/dsv-darwin-x64", "linux/amd64":"https://dsv.secretsvaultcloud.com/downloads/cli/1.29.0/dsv-linux-x64"}}`,
+			content: time.Now().Add(checkFrequencyDays*-2*time.Hour*24).Format(dateLayout) +
+				"\n" + `{"latest":"1.40.6","links": {"darwin/amd64":"https://dsv.secretsvaultcloud.com/downloads/cli/1.40.6/dsv-darwin-x64", "linux/amd64":"https://dsv.secretsvaultcloud.com/downloads/cli/1.40.6/dsv-linux-x64"}}` + "\n\n",
 			result: nil,
 		},
 		{
 			name:     "Correct case",
 			filename: "correct_case_*.json",
-			content: time.Now().Add(-1*(checkFrequencyDays-1)*time.Hour*24).Format(dateLayout) + `
-		{"latest":"1.29.0","links": {"darwin/amd64":"https://dsv.secretsvaultcloud.com/downloads/cli/1.29.0/dsv-darwin-x64"}}` +
-				"\n",
+			content: time.Now().Add(-1*(checkFrequencyDays-1)*time.Hour*24).Format(dateLayout) +
+				"\n" + `{"latest":"1.40.6","links": {"darwin/amd64":"https://dsv.secretsvaultcloud.com/downloads/cli/1.40.6/dsv-darwin-x64"}}` + "\n\n",
 			result: &latestInfo{
-				Latest: "1.29.0",
-				Links:  map[string]string{"darwin/amd64": "https://dsv.secretsvaultcloud.com/downloads/cli/1.29.0/dsv-darwin-x64"},
+				Latest: "1.40.6",
+				Links:  map[string]string{"darwin/amd64": "https://dsv.secretsvaultcloud.com/downloads/cli/1.40.6/dsv-darwin-x64"},
 			},
 		},
 	}
+	createTestDirectory(t)
 	t.Parallel()
-
 	for _, tc := range testCases {
 		testCase := tc
 		t.Run(testCase.name, func(t *testing.T) {
 			t.Parallel()
 			fName := "some_not_existed_file"
 			if testCase.filename != "" {
-				temporaryFile, err := os.CreateTemp("", testCase.filename)
+				temporaryFile, err := os.CreateTemp(cacheDirectory, testCase.filename)
 				assert.NoError(t, err)
 				defer func(name string) {
-					err := os.Remove(name)
-					if err != nil {
-						t.Logf("error during delete temporary file: %s", err)
+					if os.Getenv("KEEP_FILES") != "" {
+						t.Log("KEEP_FILES was set, so local test files are kept for debugging")
+					} else {
+						if err := os.Remove(name); err != nil {
+							t.Logf("error during delete temporary file: %s", err)
+						}
 					}
 				}(temporaryFile.Name())
-				_, err = temporaryFile.Write([]byte(testCase.content))
+
+				_, err = temporaryFile.WriteString(testCase.content) // not sure why this keeps going away during fmt
 				assert.NoError(t, err)
 
 				err = temporaryFile.Close()
@@ -140,13 +157,21 @@ func TestFetchContent(t *testing.T) {
 
 func TestUpdateCache(t *testing.T) {
 	content := []byte("some content")
-
-	temporaryDirName, err := os.MkdirTemp("", "")
+	createTestDirectory(t)
+	temporaryDirName, err := os.MkdirTemp(cacheDirectory, "")
 	assert.NoError(t, err)
 	defer os.Remove(temporaryDirName)
 
 	temporaryFileName := filepath.Join(temporaryDirName, cacheFileName)
-	defer os.Remove(temporaryFileName)
+	defer func() {
+		if os.Getenv("KEEP_FILES") != "" {
+			t.Log("KEEP_FILES was set, so local test files are kept for debugging")
+		} else {
+			if err := os.Remove(temporaryFileName); err != nil {
+				t.Logf("error during delete temporary file: %s", err)
+			}
+		}
+	}()
 
 	updateCache(temporaryFileName, content)
 
